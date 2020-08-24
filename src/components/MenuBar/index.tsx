0d8c00308d6857c2f7048cb5c761b5b6a6667c85
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-08-23 21:26:20 -0300

    chore: added component MenuBar

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Button from '../Button';
+
+import {
+  Container,
+  Topside,
+  Logo,
+  MenuButton,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  ExitIcon,
+  BotSide,
+  Avatar,
+  ProfileData,
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <Topside>
+        <Logo />
+        <MenuButton>
+          <HomeIcon />
+          <span>Página Inicial</span>
+        </MenuButton>
+        <MenuButton>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButton>
+        <MenuButton>
+          <EmailIcon />
+          <span>Mensagens</span>
+        </MenuButton>
+        <MenuButton>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButton>
+        <MenuButton>
+          <ProfileIcon className="active" />
+          <span>Perfil</span>
+        </MenuButton>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </Topside>
+      <BotSide>
+        <Avatar />
+        <ProfileData>
+          <strong>Guilherme Rodz</strong>
+          <span>@guilherme_rodz</span>
+        </ProfileData>
+        <ExitIcon />
+      </BotSide>
+    </Container>
+  );
+};
+
+export default MenuBar;
